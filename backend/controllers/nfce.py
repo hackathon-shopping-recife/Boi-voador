18e677e7bb4c32cbe44bcbc252953eafedf3aa37
Ramon Henrique <rhpr@cin.ufpe.br>
2018-09-29 16:45:15 -0300

    Add JsonResponse

--- backend/controllers/nfce.py
+++ backend/controllers/nfce.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
+def decodeNfce(link):
+    # TODO: Put here the functions to use crawler
+    return {'hello': 'nfce', 'link': link}
+
 class Nfce(View):
 
     # Hack to allow use POST without csrf field
@@ -12,6 +16,11 @@ class Nfce(View):
         return super(Nfce, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        return HttpResponse('NFCE')
+        date = request.GET.get('date')
+        cpf = request.GET.get('cpf')
+        return JsonResponse()
+
     def post(self, request):
-        pass
+        cpf = request.POST.get('cpf')
+        link = request.POST.get('link')
+        return JsonResponse(decodeNfce(link))
