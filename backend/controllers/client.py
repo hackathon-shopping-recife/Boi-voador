aa4dc0168cbbd03bae7dc93e5c7e59d5b6770211
Ramon Henrique <rhpr@cin.ufpe.br>
2018-09-29 16:40:55 -0300

    Fix POST requests

--- backend/controllers/client.py
+++ backend/controllers/client.py
@@ -5,5 +5,10 @@ from django.http import HttpResponse
 class Client(View):
     def get(self, request):
         return HttpResponse('Client bla')
+    # Hack to allow use POST without csrf field
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(Client, self).dispatch(request, *args, **kwargs)
+
     def post(self, request):
         pass
--- backend/controllers/nfce.py
+++ backend/controllers/nfce.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 class Nfce(View):
+
+    # Hack to allow use POST without csrf field
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(Nfce, self).dispatch(request, *args, **kwargs)
+
     def get(self, request):
         return HttpResponse('NFCE')
     def post(self, request):
